b74f92253524badaae4cf9efa8605e7add4b0b34
toeknee919 <toeknee919@gmail.com>
2015-02-05 14:55:43 -0800

    some more editor functionality

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -2,3 +2,8 @@
 $(document).ready(function(){
 	console.log('this is the editor view');
 });
+
+//changes the script in the database on submit
+function split_modal(){
+	
+}
--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,7 +1,30 @@
 class EditorController < ApplicationController
 	def index
 		@work = Work.find_by_id(params['work'])
-		@text = Text.all.where(work: params[:work])
+		@text = Text.all.where(work: params[:work]) #all the text lines
+		#@size = Text.all.where(work: params[:work]).count
+
+		@a = Array.new
+
+		textLines = Text.where(work: params[:work])
+		puts "textLines" + textLines.to_s
+		
+		textLines.each do |name|
+		#gives each elements info
+			hash_lines = {}#{:textLineSeq =>" ", :textLineId =>" ", :textText =>" ", :vis =>" ",:charName =>" ", :charId =>" "}
+			puts "name: ***** " + name.to_s
+			ch = Element.find_by(id: name.element_id)
+			hash_lines[:textText] = name.content_text
+			hash_lines[:textLineSeq] = name.sequence
+			hash_lines[:textLineId] = name.id
+			hash_lines[:charName] = ch.element_name
+			hash_lines[:charId] = ch.id
+			hash_lines[:vis] = name.visibility
+
+			#push hash into array
+			@a << hash_lines
+
+		end
 		# render json: @text
 	end
 
@@ -14,8 +37,9 @@ class EditorController < ApplicationController
 		end
 	end
 
-	def edit
+	def edit 
 		texts = Text.find_by(sequence: params['id'])
+		puts texts.class.name
 		if texts.class.name != 'Text'
 			texts.each do |text|
 				if text.work_id == params['work']
@@ -31,4 +55,30 @@ class EditorController < ApplicationController
 			edit.js
 		end
 	end
+
+=begin
+	def edit_stuff
+		 @a = Array.new
+
+		 hash_lines = {:textLineSeq =>" ", :textLineId =>" ", :textText =>" ", :vis =>" ",:charName =>" ", :charId =>" "}
+
+		 textLines = Text.where(work: params[:work])
+
+		 textLines.each do |name|
+		 #gives each elements info
+		 	ch = Element.find_by(id: name.element_id)
+		 	hash_lines[:textText] = name.content_text
+		 	hash_lines[:textLineSeq] = name.sequence
+		 	hash_lines[:textLineId] = name.id
+		 	hash_lines[:charName] = ch.element_name
+		 	hash_lines[:charId] = ch.id
+		 	hash_lines[:vis] = name.visibility
+
+		 	#push hash into array
+		 	@a.push(hash_lines)
+		end
+		
+	end
+=end
+
 end
